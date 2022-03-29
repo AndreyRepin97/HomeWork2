c8013a6b796aa85d778fa5a5c2a39ec1baeb672d
porwa <o-la-la-repka@yandex.ru>
2022-03-29 21:07:48 +0300

    HomeWork2_task_four

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,12 +5,11 @@ import java.awt.desktop.SystemSleepListener;
 public class Main {
 
     public static void main(String[] args) {
-short bananas = 5;
-float milk = 200F;
-short plombir = 2;
-short eggs = 4;
-float finalWeight = bananas*80 + milk/100*105 + plombir*100 + eggs*70;
-finalWeight/= 1000;
-System.out.println("Итоговый вес, кг " + finalWeight);
+short daysWeightLostMin = 7*1000/250;
+short daysWeightLostMax = 7*1000/500;
+System.out.println("Минимум нужно " + daysWeightLostMax +" дней.");
+System.out.println("Максимум нужно " + daysWeightLostMin +" дней.");
+int daysWeightLostOverage = (daysWeightLostMin + daysWeightLostMax)/2;
+System.out.println("В среднем нужно " + daysWeightLostOverage +" дней.");
     }
 }
