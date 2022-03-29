dd2cb47060b2fefe70e54253b37ba81892e58425
porwa <o-la-la-repka@yandex.ru>
2022-03-29 20:53:25 +0300

    HomeWork2_task_three_changed

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 short bananas = 5;
-short milk = 200;
+float milk = 200F;
 short plombir = 2;
 short eggs = 4;
 float finalWeight = bananas*80 + milk/100*105 + plombir*100 + eggs*70;
