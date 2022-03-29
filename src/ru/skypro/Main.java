ef35ea8b1a8c346c006a601294b6e85e43b14d52
porwa <o-la-la-repka@yandex.ru>
2022-03-29 20:52:25 +0300

    HomeWork2_task_three

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,16 +5,12 @@ import java.awt.desktop.SystemSleepListener;
 public class Main {
 
     public static void main(String[] args) {
-float firstBoxer = 78.2F;
-float secondBoxer = 82.7F;
-float boxersWeight = secondBoxer + firstBoxer;
-float boxersWeightDifference1 = secondBoxer - firstBoxer;
-float boxersWeightDifference2 = secondBoxer%firstBoxer;
-        System.out.println("Вес боксеров");
-        System.out.println(boxersWeight);
-        System.out.println("Разность веса боксеров");
-        System.out.println(boxersWeightDifference1);
-        System.out.println("Другая разность веса боксеров");
-        System.out.println(boxersWeightDifference2);
+short bananas = 5;
+short milk = 200;
+short plombir = 2;
+short eggs = 4;
+float finalWeight = bananas*80 + milk/100*105 + plombir*100 + eggs*70;
+finalWeight/= 1000;
+System.out.println("Итоговый вес, кг " + finalWeight);
     }
 }
