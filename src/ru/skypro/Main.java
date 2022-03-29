90e46d2838da8731c345bc272e114a80ce8c7293
porwa <o-la-la-repka@yandex.ru>
2022-03-29 21:25:26 +0300

    HomeWork2_task_five

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,11 +5,20 @@ import java.awt.desktop.SystemSleepListener;
 public class Main {
 
     public static void main(String[] args) {
-short daysWeightLostMin = 7*1000/250;
-short daysWeightLostMax = 7*1000/500;
-System.out.println("Минимум нужно " + daysWeightLostMax +" дней.");
-System.out.println("Максимум нужно " + daysWeightLostMin +" дней.");
-int daysWeightLostOverage = (daysWeightLostMin + daysWeightLostMax)/2;
-System.out.println("В среднем нужно " + daysWeightLostOverage +" дней.");
+int MariaSalary =67_760;
+int KristinaSalary =83_690;
+int DenisSalary =76_230;
+
+int MariaFinalSalary = MariaSalary/100*10 + MariaSalary;
+int DenisFinalSalary = DenisSalary/100*10 + DenisSalary;
+int KristinaFinalSalary = KristinaSalary/100*10 + KristinaSalary;
+
+int MariaYearSalaryDif = MariaFinalSalary*12 - MariaSalary*12;
+int DenisYearSalaryDif = DenisFinalSalary*12 - DenisSalary*12;
+int KristinaYearSalaryDif = KristinaFinalSalary*12 - KristinaSalary*12;
+
+System.out.println("Маша теперь получает " + MariaFinalSalary + " рублей. Годовой доход вырос на " + MariaYearSalaryDif + " рублей.");
+System.out.println("Денис теперь получает " + DenisFinalSalary + " рублей. Годовой доход вырос на " + DenisYearSalaryDif + " рублей.");
+System.out.println("Кристина теперь получает " + KristinaFinalSalary + " рублей. Годовой доход вырос на " + KristinaYearSalaryDif + " рублей.");
     }
 }
