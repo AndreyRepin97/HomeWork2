b63afbd3035ea8a2897bc164d6ee8493cbd4bf8b
porwa <o-la-la-repka@yandex.ru>
2022-03-29 20:15:05 +0300

    HomeWork2_task_one

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,7 +3,17 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// JustForCommitTrying
-        System.out.println("Hello World");
+        //intTypes
+int i = 10000;
+byte b = 127;
+short sh = 2;
+long lng = 10_000_000_000_000_000L;
+//double point types
+float flt = 3.14F;
+double dbl = 3.1415926535;
+//specType
+char chr = 33;
+//System.out.println(chr);
+boolean bln = i>b;
     }
 }
